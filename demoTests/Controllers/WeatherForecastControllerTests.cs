d7c0119536230597c5448b5b757ba6fd46d49084
Rolsen <Bruger@LAPTOP-LHCL8MS0>
2023-03-20 13:55:18 +0100

    use swagger in git

--- demoTests/Controllers/WeatherForecastControllerTests.cs
+++ demoTests/Controllers/WeatherForecastControllerTests.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using demo.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace demoTests.Controllers
+namespace demo.Controllers.Tests
 {
-    internal class WeatherForecastControllerTests
+    [TestClass()]
+    public class WeatherForecastControllerTests
     {
+        [TestMethod()]
+        public void GetTest()
+        {
+
+            Assert.Fail();
+        }
     }
 }
